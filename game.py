526b780cd5fe301e138a45e752f9c0200edec0e8
cocenza <gabrielcocenza@gmail.com>
2019-11-27 23:54:34 -0300

    shows winner

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
 import arcade
-import random
 import os
-import threading
+import random
+import sys
 import time
+import threading
 
 COW_SCALING = 0.2
 GOAT_SCALING = 0.07
@@ -39,7 +40,6 @@ class Food(arcade.Sprite):
     food_exists = False
 
 
-
 class MyGame(arcade.Window):
 
     def __init__(self, width, height, title):
@@ -49,12 +49,10 @@ class MyGame(arcade.Window):
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
 
-        # Variables that will hold sprite lists
         self.player_list = None
         self.lettuce_list = None
         self.poop_list = None
 
-        # Set up the player info
         self.player_sprite_1 = None
         self.player_sprite_2 = None
         self.food_sprite = None
@@ -64,29 +62,27 @@ class MyGame(arcade.Window):
         self.score_1 = 0
         self.score_2 = 0
 
-        # Set the background color
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
-        """ Set up the game and initialize the variables. """
 
-        # Sprite lists
         self.player_list = arcade.SpriteList()
         self.lettuce_list = arcade.SpriteList()
         self.poop_list = arcade.SpriteList()
 
-        # Score
         self.score_1 = 0
         self.score_2 = 0
 
-
-        # Set up the player
+        #player information
         self.player_sprite_1 = Player("images/cow.png", COW_SCALING)
-        self.player_sprite_1.name = 'vaquinha'
+        self.player_sprite_1.name = 'cow'
+
         self.player_sprite_2 = Player("images/goat.png", GOAT_SCALING)
-        self.player_sprite_2.name = 'cabrinha'
+        self.player_sprite_2.name = 'goat'
+
         self.player_sprite_1.center_x = 0
         self.player_sprite_1.center_y = 0
+
         self.player_sprite_2.center_x = 800
         self.player_sprite_2.center_y = 700
 
@@ -95,22 +91,15 @@ class MyGame(arcade.Window):
 
         self.food_exists
         self.lock = threading.Lock()
-        # incio thread
+        
+        # thread begins
         self.thread = threading.Thread(target=self.food_timer)
         self.thread.start()
-
-        #Create Food
-        # for number in range(LETTUCE_COUNT):
-        #     lettuce = 'lettuce_sprite_{}'.format(number)
-        #     self.lettuce = Food("images/lettuce.png", LETTUCE_SCALING)
-        #     self.lettuce.center_x = random.randrange(SCREEN_WIDTH)
-        #     self.lettuce.center_y = random.randrange(SCREEN_HEIGHT)
-        #     self.lettuce_list.append(self.lettuce)
 
 
     def food_timer(self):
         while True:
-            time.sleep(3)
+            time.sleep(2)
             with self.lock:
                 if not self.food_exists:                    
                     self.food_exists = True
@@ -123,28 +112,26 @@ class MyGame(arcade.Window):
 
 
     def on_draw(self):
-        """
-        Render the screen.
-        """
 
-        # This command has to happen before we start drawing
         arcade.start_render()
         self.lettuce_list.draw()
         self.player_list.draw()
         self.poop_list.draw()
-
-        
+      
         score_cow = f"Score Cow: {self.score_1}"
         score_goat = f"Score Goat: {self.score_2}"
         arcade.draw_text(score_cow, 10, 20, arcade.color.WHITE, 14)
         arcade.draw_text(score_goat, 650, 20, arcade.color.WHITE, 14)
 
+        if self.score_1 == 5 or self.score_2 == 5:
+            if self.score_1 == 5:
+                arcade.draw_text("COW WINS!",280,400,arcade.color.RED, 40)
+            else:
+                arcade.draw_text("GOAT WINS!",280,400,arcade.color.RED, 40)
+
+
 
     def on_update(self, delta_time):
-        """ Movement and game logic """
-
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
 
         if not self.food_sprite:
             with self.lock:
@@ -174,7 +161,7 @@ class MyGame(arcade.Window):
                         with self.lock:
                             self.food_exists = False
                 else:
-                    if hit_list[0].name=='vaquinha':
+                    if hit_list[0].name=='cow':
                         self.score_1 += 1
                         self.food_sprite.kill()
                         self.food_sprite = None
@@ -187,17 +174,6 @@ class MyGame(arcade.Window):
                         with self.lock:
                             self.food_exists = False
 
-        #
-
-        # Loop through each colliding sprite, remove it, and add to the score.
-        # for lettuce in cow_hit_list:
-        #     lettuce.remove_from_sprite_lists()
-        #     self.score_1 += 1
-
-        # for lettuce in goat_hit_list:
-        #     lettuce.remove_from_sprite_lists()
-        #     self.score_2 += 1
-
         self.player_list.update()
         self.poop_list.update()
         if self.food_sprite:
@@ -206,7 +182,7 @@ class MyGame(arcade.Window):
 
 
     def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
+        #Player movements
 
         if key == arcade.key.UP:
             self.player_sprite_1.change_y = MOVEMENT_SPEED
@@ -221,7 +197,6 @@ class MyGame(arcade.Window):
              poop.center_x = self.player_sprite_1.center_x
              poop.center_y = self.player_sprite_1.center_y
              self.poop_list.append(poop)
-
 
         elif key == arcade.key.W:
             self.player_sprite_2.change_y = MOVEMENT_SPEED
@@ -239,8 +214,7 @@ class MyGame(arcade.Window):
 
 
     def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
-
+        # To stop moviments after release an key
         if key == arcade.key.UP or key == arcade.key.DOWN:
             self.player_sprite_1.change_y = 0
         elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
@@ -253,7 +227,6 @@ class MyGame(arcade.Window):
 
 
 def main():
-    """ Main method """
     window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
     arcade.run()
